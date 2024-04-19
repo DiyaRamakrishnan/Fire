7b1b28d96346ca53962d7c1c7318939cf8360eca
DiyaRamakrishnan <148472960+DiyaRamakrishnan@users.noreply.github.com>
2024-04-19 00:32:32 +0000

    l

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 from tensorflow.keras.models import load_model
 from info_page import show_info_page
 
-class_labels = ['DownStairs', 'UpStairs', 'Walking', 'DropTest', 'Standing']
+class_labels = ['DownStairs', 'DropTest', 'Standing', 'UpStairs', 'Walking']
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 model_file_path = os.path.join(script_dir, 'models', 'model.h5')
